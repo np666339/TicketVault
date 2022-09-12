10384e2320f686826c6779830216d42ccb155db9
np666339 <nopost22@gmail.com>
2022-09-11 22:57:48 -0500

    First commit

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import concert from './assets/concert.png';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
+
+const App = () => {
+  const [ticket, setTicket] = useState(0),
+  [total, setTotal] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{paddingBottom: 30, fontSize: 50}}>Ticket Vault</Text>
+      <TextInput
+        style={styles.textPut}
+        placeholder="Number of Tickets"
+        onChangeText={newText => setTicket(newText)}
+      />
+
+      <Text style={{padding: 5}}> </Text>
+
+      <TouchableOpacity style={styles.button}
+        onPress={() => {setTotal (parseFloat((ticket * 99.99).toFixed(2)));}}>
+        <Text style={styles.buttonText}> Calculate </Text>
+       </TouchableOpacity>
+
+        <Text style={{padding: 50, fontSize: 25}}>
+          Ticket Cost: ${total}
+        </Text>
+
+        <Image source={concert} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +42,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textPut: {
+    backgroundColor: 'white',
+    paddingLeft: 60,
+    paddingRight: 60,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderWidth: 2,
+  },
+  button: {
+    borderRadius: 5,
+    paddingLeft: 60,
+    paddingRight: 60,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#f98b88'
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black'
+  }
 });
+
+export default App;
